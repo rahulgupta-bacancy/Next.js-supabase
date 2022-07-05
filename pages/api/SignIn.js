5a028d779df4c96c2ec9171ef1ed7a5eaddeb79e
rahulgupta-bacancy <rahul.gupta@bacancy.com>
2022-07-05 18:26:29 +0530

    SignIn Changes

--- pages/api/SignIn.js
+++ pages/api/SignIn.js
@@ -1,7 +1,8 @@
 import supabase from "../../utils/supabase";
 module.exports = async (req, res) => {
   const email = req.body.email;
-  const data = await supabase.auth.signIn({ email });
+  const password = req.body.password;
+  const data = await supabase.auth.signIn({ email, password });
   console.log(req.body.email);
   return res.send(data);
 };
